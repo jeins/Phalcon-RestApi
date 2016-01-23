d94673bb7720fb8b32b3f79105d5d48098fbab66
jeins <mail@mjuanakbar.info>
2016-01-23 18:32:59 +0100

    set db debug

--- Config/AppService.php
+++ Config/AppService.php
@@ -8,6 +8,8 @@ use Phalcon\Di\FactoryDefault;
 use Phalcon\Mvc\Url;
 use Phalcon\Db\Adapter\Pdo\Mysql as DatabaseConnection;
 use Resapi\Route\RouteLoader;
+use Phalcon\Events\Manager as EventsManager;
+use Phalcon\Logger\Adapter\File as FileLogger;
 
 require __DIR__ . '/AppLoader.php';
 
@@ -54,7 +56,24 @@ class AppService extends AppLoader
         $connection = new DatabaseConnection($this->database->toArray());
         $debug = $this->application->debug;
         if($debug){
-            //SETUP DEBUG
+            $eventsManager = new EventsManager();
+            $logger = new FileLogger(__DIR__ . "/../Logs/db.log");
+
+            //Listen all the database events
+            $eventsManager->attach(
+                'db',
+                function($event, $connection) use($logger){
+                    if($event->getType() == 'beforeQuery'){
+                        $variables = $connection->getSQLVariables();
+                        if ($variables) {
+                            $logger->log($connection->getSQLStatement() . ' [' . join(',', $variables) . ']', \Phalcon\Logger::INFO);
+                        } else {
+                            $logger->log($connection->getSQLStatement(), \Phalcon\Logger::INFO);
+                        }
+                    }
+                }
+            );
+            $connection->setEventsManager($eventsManager);
         }
 
         return $connection;
